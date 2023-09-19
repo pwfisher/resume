cc51dc7605a0523b250905fd162f2739e9f46660
Patrick Fisher <patrick@pwfisher.com>
2023-09-18 17:08:49 -0700

    feat: add Jobs section header

--- src/components/Jobs/Jobs.tsx
+++ src/components/Jobs/Jobs.tsx
@@ -5,6 +5,7 @@ import styles from './Jobs.module.css'
 
 export const Jobs: FC<{ jobs: IJob[] }> = ({ jobs }) => (
   <div className={styles.body}>
+    <h2 className={styles.header}>Experience</h2>
     {[...jobs].reverse().map(job => (
       <Job key={job.id} {...{ job }} />
     ))}
